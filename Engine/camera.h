3694c40e08d1cf855f53c5fde187ba0fbda508e6
jakokorina <73951854+jakokorina@users.noreply.github.com>
2021-05-22 14:30:35 +0300

    Update camera.h
    fixed camera

--- Engine/camera.h
+++ Engine/camera.h
@@ -3,6 +3,7 @@
 #include "screen.h"
 #include <glm/vec3.hpp>
 #include <glm/matrix.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 class Camera {
 private:
@@ -27,16 +28,15 @@ void Camera::setDirection(glm::vec3 new_direction) {
 }
 
 glm::mat4 Camera::getCameraTransformationMatrix() const {
-    glm::vec3 viewUpVector = direction;
-    viewUpVector.y += 1;
+    glm::vec3 viewUpVector = glm::vec3{0,1.f,0};
     std::vector<glm::vec3> orthonormalCameraBasis(3);
-    orthonormalCameraBasis[0] = direction;
-    orthonormalCameraBasis[1] = glm::cross(direction, viewUpVector);
-    orthonormalCameraBasis[2] = glm::cross(orthonormalCameraBasis[1], direction);
-    for (auto & vec : orthonormalCameraBasis) {
+    orthonormalCameraBasis[2] = -direction;
+    orthonormalCameraBasis[0] = glm::cross(direction, viewUpVector);
+    orthonormalCameraBasis[1] = glm::cross(orthonormalCameraBasis[0], direction);
+    for (auto& vec : orthonormalCameraBasis) {
         vec /= vec.length();
     }
-    orthonormalCameraBasis[0] = -orthonormalCameraBasis[0];
+    // orthonormalCameraBasis[0] = -orthonormalCameraBasis[0];
     glm::mat4 transformMatrix = glm::mat4(0.f);
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j) {
