26bc0725b4954df008690976b4d242d1095fb959
jakokorina <73951854+jakokorina@users.noreply.github.com>
2021-05-27 23:02:14 +0300

    cpp files

---
+++ Engine/renderer.cpp
@@ -0,0 +1,20 @@
+#include "renderer.h"
+
+namespace Engine {
+    void Renderer::drawObjects(const std::vector<Triangle>& objects, sf::RenderWindow& window) {
+        glm::mat4 transformMatrix =
+                screen.getViewOrtMatrix() * screen.getProjectionMatrix() * camera.getCameraTransformationMatrix();
+        for (const auto& object : objects) {
+            object.draw(screen, transformMatrix);
+        }
+        screen.screenToWindow(window);
+        {
+            auto[h, w] = screen.getSize();
+            sf::RectangleShape pixel;
+            pixel.setSize({10.f, 10.f});
+            pixel.setPosition({(float) w / 2, (float) h / 2});
+            pixel.setFillColor(sf::Color::White);
+            window.draw(pixel);
+        }
+    }
+}
