8d093edeb4ecd6e6d21335b677601f9905be476b
jakokorina <73951854+jakokorina@users.noreply.github.com>
2021-05-27 22:38:22 +0300

    namespace added

--- Engine/renderer.h
+++ Engine/renderer.h
@@ -6,20 +6,32 @@
 #include "screen.h"
 #include <vector>
 
-class Renderer{
-private:
-    Camera& camera;
-    Screen& screen;
+namespace Engine {
 
-public:
-    Renderer(Screen &scr, Camera &cam): screen(scr), camera(cam) {}
-    void drawObjects(const std::vector<Triangle>& objects, sf::RenderWindow& window);
-};
+    class Renderer {
+    private:
+        Camera& camera;
+        Screen& screen;
 
-void Renderer::drawObjects(const std::vector<Triangle>& objects, sf::RenderWindow& window) {
-    glm::mat4 transformMatrix = screen.getVeiwOrtMatrix() * screen.getProjectionMatrix() * camera.getCameraTransformationMatrix();
-    for (const auto& object : objects) {
-        object.draw(screen, transformMatrix);
+    public:
+        Renderer(Screen& scr, Camera& cam) : screen(scr), camera(cam) {}
+        void drawObjects(const std::vector<Triangle>& objects, sf::RenderWindow& window);
+    };
+
+    void Renderer::drawObjects(const std::vector<Triangle>& objects, sf::RenderWindow& window) {
+        glm::mat4 transformMatrix =
+                screen.getViewOrtMatrix() * screen.getProjectionMatrix() * camera.getCameraTransformationMatrix();
+        for (const auto& object : objects) {
+            object.draw(screen, transformMatrix);
+        }
+        screen.screenToWindow(window);
+        {
+            auto[h, w] = screen.getSize();
+            sf::RectangleShape pixel;
+            pixel.setSize({10.f, 10.f});
+            pixel.setPosition({(float) w / 2, (float) h / 2});
+            pixel.setFillColor(sf::Color::White);
+            window.draw(pixel);
+        }
     }
-    screen.screenToWindow(window);
 }
