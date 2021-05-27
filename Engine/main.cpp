211d34d303dcc4477a7e8305e564e15bb54dc6a3
jakokorina <73951854+jakokorina@users.noreply.github.com>
2021-05-27 22:40:09 +0300

    Update main.cpp

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -2,9 +2,9 @@
 #include "world.h"
 
 int main() {
-    World W(800, 600);
-    Triangle T({{1,1,1}, {-1, -1, 1}, {-1, 1, 1}}, sf::Color::Red);
-    W.addObject(T);
+    Engine::World W(1800, 1000);
+    W.addObject({10,10,5}, {-10, -10, 5}, {10, -10, 5}, sf::Color::Red);
+    W.addObject({1,1,0.5}, {-1, -1, 0.5}, {1, -1, 0.5}, sf::Color::Green);
     W.start();
     return 0;
 }
