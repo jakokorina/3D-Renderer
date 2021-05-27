62d13fe36f8d94d661d79464eac24883ba12e56f
jakokorina <73951854+jakokorina@users.noreply.github.com>
2021-05-27 23:03:12 +0300

    splitting into declaration and definition

--- Engine/renderer.h
+++ Engine/renderer.h
@@ -17,21 +17,4 @@ namespace Engine {
         Renderer(Screen& scr, Camera& cam) : screen(scr), camera(cam) {}
         void drawObjects(const std::vector<Triangle>& objects, sf::RenderWindow& window);
     };
-
-    void Renderer::drawObjects(const std::vector<Triangle>& objects, sf::RenderWindow& window) {
-        glm::mat4 transformMatrix =
-                screen.getViewOrtMatrix() * screen.getProjectionMatrix() * camera.getCameraTransformationMatrix();
-        for (const auto& object : objects) {
-            object.draw(screen, transformMatrix);
-        }
-        screen.screenToWindow(window);
-        {
-            auto[h, w] = screen.getSize();
-            sf::RectangleShape pixel;
-            pixel.setSize({10.f, 10.f});
-            pixel.setPosition({(float) w / 2, (float) h / 2});
-            pixel.setFillColor(sf::Color::White);
-            window.draw(pixel);
-        }
-    }
 }
