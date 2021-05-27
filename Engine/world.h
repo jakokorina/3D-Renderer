a5196d1faa20246c65fbab18fe25692a08bfb9b0
jakokorina <73951854+jakokorina@users.noreply.github.com>
2021-05-27 23:04:28 +0300

    splitting into declaration and definition

--- Engine/world.h
+++ Engine/world.h
@@ -38,40 +38,4 @@ namespace Engine {
         void addObject(const glm::vec3& p1, const glm::vec3& p2, const glm::vec3& p3, sf::Color colour);
         void start();
     };
-
-    void World::addObject(const std::vector<glm::vec3>& points,  sf::Color colour) {
-        Triangle triangle(points, colour);
-        objects.push_back(triangle);
-    }
-    void World::addObject(const glm::vec3& p1, const glm::vec3& p2, const glm::vec3& p3,  sf::Color colour) {
-        Triangle triangle(p1, p2, p3, colour);
-        objects.push_back(triangle);
-    }
-
-    void World::setCameraLocation(glm::vec3 new_location) {
-        camera.setLocation(new_location);
-    }
-
-    void World::drawPicture() {
-        window.clear();
-        screen.clear();
-        renderer.drawObjects(objects, window);
-        window.display();
-    }
-
-    void World::start() {
-        drawPicture();
-        while (window.isOpen()) {
-            sf::Event event;
-            while (window.pollEvent(event)) {
-                // Request for closing the window
-                if (event.type == sf::Event::Closed)
-                    window.close();
-            }
-            // здесь можно написать обработку движения камеры с клавиатуры
-        }
-    }
-    void World::setCameraDirection(glm::vec3 new_direction) {
-        camera.setDirection(new_direction);
-    }
 }
