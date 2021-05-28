21f0c67149e5f57dd42eebe2131fc959206fa93d
jakokorina <73951854+jakokorina@users.noreply.github.com>
2021-05-28 15:02:31 +0300

    refactoring code

--- Engine/screen.h
+++ Engine/screen.h
@@ -2,6 +2,7 @@
 
 #include <SFML/Graphics.hpp>
 #include <glm/vec3.hpp>
+#include <glm/matrix.hpp>
 #include <utility>
 #include <vector>
 
@@ -13,14 +14,18 @@ namespace Engine {
         float left = -2, right = 2, top = 1, bottom = -1;
         std::vector<std::vector<sf::Color>> colourBuf;
         std::vector<std::vector<float>> zBuf;
+        glm::mat4 projectionMatrix;
+        glm::mat4 viewOrtMatrix;
+        void computeViewOrt();
+        void computeProjection();
 
     public:
         Screen(int h, int w);
         std::pair<size_t, size_t> getSize() const;
         glm::mat4 getProjectionMatrix() const;
+        glm::mat4 getViewOrtMatrix() const;
         void setColour(sf::Color colour, size_t x, size_t y, float z);
         void screenToWindow(sf::RenderWindow& window);
         void clear();
-        glm::mat4 getViewOrtMatrix() const;
     };
 }
