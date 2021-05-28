cc1df43285ca0885648201cb59e7276df6a32320
jakokorina <73951854+jakokorina@users.noreply.github.com>
2021-05-28 15:02:09 +0300

    refactoring code

--- Engine/screen.cpp
+++ Engine/screen.cpp
@@ -1,17 +1,20 @@
 #include "screen.h"
 
 namespace Engine {
+    Screen::Screen(int h, int w) : height(h), width(w) {
+        colourBuf = std::vector<std::vector<sf::Color>>(w, std::vector<sf::Color>(h, sf::Color::Black));
+        zBuf = std::vector<std::vector<float>>(w, std::vector<float>(h, far + 1));
+    }
+
     std::pair<size_t, size_t> Screen::getSize() const {
         return std::pair<size_t, size_t>(height, width);
     }
 
     glm::mat4 Screen::getProjectionMatrix() const {
-        glm::mat4 projection(near);
-        projection[2][2] = (far + near) / (far - near);
-        projection[3][3] = 0;
-        projection[2][3] = -1;
-        projection[3][2] = 2 * far * near / (far - near);
-        return projection;
+        return projectionMatrix;
+    }
+    glm::mat4 Screen::getViewOrtMatrix() const {
+        return viewOrtMatrix;
     }
 
     void Screen::setColour(sf::Color colour, size_t x, size_t y, float z) {
@@ -24,7 +27,6 @@ namespace Engine {
             colourBuf[width - 1 - x][height - 1 - y] = colour;
         }
     }
-
     void Screen::screenToWindow(sf::RenderWindow& window) {
         for (int x = 0; x < width; ++x) {
             for (int y = 0; y < height; ++y) {
@@ -45,7 +47,7 @@ namespace Engine {
             }
     }
 
-    glm::mat4 Screen::getViewOrtMatrix() const {
+    void Screen::computeViewOrt() {
         glm::mat4 viewport(1);
         viewport[3][0] = (width - 1) / 2.f;
         viewport[3][1] = (height - 1) / 2.f;
@@ -57,11 +59,14 @@ namespace Engine {
         ort[2][2] = -1;
         ort[3][0] = -(right + left) / (right - left);
         ort[3][1] = -(top + bottom) / (top - bottom);
-        return viewport * ort;
+        viewOrtMatrix = viewport * ort;
     }
-
-    Screen::Screen(int h, int w) : height(h), width(w) {
-        colourBuf = std::vector<std::vector<sf::Color>>(w, std::vector<sf::Color>(h, sf::Color::Black));
-        zBuf = std::vector<std::vector<float>>(w, std::vector<float>(h, far + 1));
+    void Screen::computeProjection() {
+        glm::mat4 projection(near);
+        projection[2][2] = (far + near) / (far - near);
+        projection[3][3] = 0;
+        projection[2][3] = -1;
+        projection[3][2] = 2 * far * near / (far - near);
+        projectionMatrix = projection;
     }
 }
