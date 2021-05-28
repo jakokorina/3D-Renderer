b6d3b600580b553c1726776caf2b4a1306d67470
jakokorina <73951854+jakokorina@users.noreply.github.com>
2021-05-28 16:04:03 +0300

    Simplified matrixes

--- Engine/screen.h
+++ Engine/screen.h
@@ -14,16 +14,14 @@ namespace Engine {
         float left = -2, right = 2, top = 1, bottom = -1;
         std::vector<std::vector<sf::Color>> colourBuf;
         std::vector<std::vector<float>> zBuf;
-        glm::mat4 projectionMatrix;
-        glm::mat4 viewOrtMatrix;
-        void computeViewOrt();
-        void computeProjection();
+        glm::mat4 screenMatrix;
+        glm::mat4 computeViewOrt();
+        glm::mat4 computeProjection();
 
     public:
         Screen(int h, int w);
         std::pair<size_t, size_t> getSize() const;
-        glm::mat4 getProjectionMatrix() const;
-        glm::mat4 getViewOrtMatrix() const;
+        glm::mat4 getScreenMatrix() const;
         void setColour(sf::Color colour, size_t x, size_t y, float z);
         void screenToWindow(sf::RenderWindow& window);
         void clear();
