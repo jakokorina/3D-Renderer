b6c11b0b838c005c1557c74f8d2ec742a3bed70b
jakokorina <73951854+jakokorina@users.noreply.github.com>
2021-05-28 15:05:12 +0300

    refactoring code

--- Engine/camera.h
+++ Engine/camera.h
@@ -3,7 +3,6 @@
 #include "screen.h"
 #include <glm/vec3.hpp>
 #include <glm/matrix.hpp>
-#include <glm/gtc/matrix_transform.hpp>
 
 
 namespace Engine {
@@ -11,14 +10,16 @@ namespace Engine {
     private:
         glm::vec3 location;
         glm::vec3 direction;
+        glm::mat4 transformMatrix;
+        void computeMatrix();
 
     public:
-        Camera(glm::vec3 loc, glm::vec3 dir) : location(loc), direction(dir) {}
+        Camera(glm::vec3 loc, glm::vec3 dir);
         Camera() = delete;
 
-        void setLocation(glm::vec3 new_location);
-        void setDirection(glm::vec3 new_direction);
+        void changeLocation(glm::vec3 delta);
+        void changeDirection(glm::vec3 delta);
 
-        glm::mat4 getCameraTransformationMatrix() const;
+        glm::mat4 getCameraTransformMatrix() const;
     };
 }
