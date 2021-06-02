d7e74da79b7827c0a7b3efb1faadfb8c79cb5148
jakokorina <73951854+jakokorina@users.noreply.github.com>
2021-06-02 19:58:40 +0300

    Colour interpolation added

--- Engine/triangle.h
+++ Engine/triangle.h
@@ -12,12 +12,15 @@ namespace Engine {
     class Triangle {
     private:
         std::vector<glm::vec3> points;
-        sf::Color colour;
-        static std::pair<bool, float> barycentricCoordinates(const glm::mat3x4& V, const glm::vec2& P);
+        std::vector<sf::Color> colour;
+        std::pair<bool, std::pair<float, sf::Color>> barycentricCoordinates(const glm::mat3x4& V, const glm::vec2& P) const;
+        static sf::Color multiplicate(sf::Color color, float f) ;
 
     public:
         Triangle(const std::vector<glm::vec3>& p, sf::Color colour_);
+        Triangle(const std::vector<glm::vec3>& p, const std::vector<sf::Color>& colour_);
         Triangle(const glm::vec3& p1, const glm::vec3& p2, const glm::vec3& p3, sf::Color colour_);
+        Triangle(const glm::vec3& p1, const glm::vec3& p2, const glm::vec3& p3, const std::vector<sf::Color>& colour_);
         void draw(Screen& screen, const glm::mat4& transformMatrix) const;
     };
 }
