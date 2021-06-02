a663cc7edbf79062c42035f21fc86a2874e1d14f
jakokorina <73951854+jakokorina@users.noreply.github.com>
2021-06-02 19:58:16 +0300

    Colour interpolation added

--- Engine/triangle.cpp
+++ Engine/triangle.cpp
@@ -4,9 +4,23 @@ namespace Engine {
     Triangle::Triangle(const std::vector<glm::vec3>& p, sf::Color colour_) {
         assert(p.size() == 3);
         points = p;
+        colour = std::vector<sf::Color>(3, colour_);
+    }
+
+    Triangle::Triangle(const std::vector<glm::vec3>& p, const std::vector<sf::Color>& colour_) {
+        assert(p.size() == 3 && colour_.size() == 3);
+        points = p;
         colour = colour_;
     }
+
     Triangle::Triangle(const glm::vec3& p1, const glm::vec3& p2, const glm::vec3& p3, sf::Color colour_) {
+        points = {p1, p2, p3};
+        colour = std::vector<sf::Color>(3, colour_);
+    }
+
+    Triangle::Triangle(const glm::vec3& p1, const glm::vec3& p2, const glm::vec3& p3,
+                       const std::vector<sf::Color>& colour_) {
+        assert(colour_.size() == 3);
         points = {p1, p2, p3};
         colour = colour_;
     }
@@ -42,19 +56,24 @@ namespace Engine {
                 } // точка не попадает в минимальный прямоугольник, содержащий наш треугольник
                 auto[inTriangle, zValue] = barycentricCoordinates(newPoints, glm::vec2{x, y});
                 if (inTriangle) {
-                    screen.setColour(colour, x, y, zValue);
+                    screen.setColour(zValue.second, x, y, zValue.first);
                 }
             }
         }
     }
 
-    std::pair<bool, float> Triangle::barycentricCoordinates(const glm::mat3x4& V, const glm::vec2& P) {
+    std::pair<bool, std::pair<float, sf::Color>>
+    Triangle::barycentricCoordinates(const glm::mat3x4& V, const glm::vec2& P) const {
         glm::vec3 a = {V[1].x - V[0].x, V[2].x - V[0].x, V[0].x - P.x};
         glm::vec3 b = {V[1].y - V[0].y, V[2].y - V[0].y, V[0].y - P.y};
         glm::vec3 c = glm::cross(a, b);
         c /= c.z;
         c.z = 1 - c.x - c.y;
         glm::vec4 point = c.z * V[0] + c.x * V[1] + c.y * V[2];
-        return {c.x >= 0 && c.y >= 0 && c.z >= 0, point.z};
+        sf::Color col = multiplicate(colour[0], c.z) + multiplicate(colour[1], c.x) + multiplicate(colour[2], c.y);
+        return {c.x >= 0 && c.y >= 0 && c.z >= 0, {point.z, col}};
+    }
+    sf::Color Triangle::multiplicate(sf::Color color, float f) {
+        return sf::Color(color.r * f, color.g * f, color.b * f, (color.a * f));
     }
 }
