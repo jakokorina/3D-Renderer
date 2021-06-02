91311cac06992b724f20a57d90dc8ecfce59c9d5
jakokorina <73951854+jakokorina@users.noreply.github.com>
2021-06-02 19:59:48 +0300

    New constructors

--- Engine/world.h
+++ Engine/world.h
@@ -6,10 +6,10 @@
 #include "camera.h"
 #include "screen.h"
 #include "triangle.h"
+#include <math.h>
 #include <vector>
 
 namespace Engine {
-
     class World {
     private:
         std::vector<Triangle> objects;
@@ -20,21 +20,15 @@ namespace Engine {
         void drawPicture();;
 
     public:
-        World(int width = 800, int height = 800, glm::vec3 camLoc = {0, 0, -1}, glm::vec3 camDir = {0, 0, 1}) : camera(
-                camLoc, camDir),
-                                                                                                                screen(height,
-                                                                                                                       width),
-                                                                                                                renderer(
-                                                                                                                        screen,
-                                                                                                                        camera),
-                                                                                                                window(sf::RenderWindow(
-                                                                                                                        sf::VideoMode(
-                                                                                                                                width,
-                                                                                                                                height),
-                                                                                                                        "Simple renderer")) {
+        World(int width = 800, int height = 800, glm::vec3 camLoc = {0, 0, -1}, glm::vec3 camDir = {0, 0, 1}) :
+                camera(camLoc, camDir),
+                screen(height, width),
+                renderer(screen, camera),
+                window(sf::RenderWindow(sf::VideoMode(width, height), "Simple renderer")) {
         }
         void addObject(const std::vector<glm::vec3>& points, sf::Color colour);
         void addObject(const glm::vec3& p1, const glm::vec3& p2, const glm::vec3& p3, sf::Color colour);
+        void addObject(const glm::vec3& p1, const glm::vec3& p2, const glm::vec3& p3, const std::vector<sf::Color>& colour);
         void start();
         void keyPressed(sf::Keyboard::Key key);
     };
