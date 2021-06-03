616e446078acc2229630e8fc0991beacaddf07b7
jakokorina <73951854+jakokorina@users.noreply.github.com>
2021-06-03 20:38:58 +0300

    changed movement

--- Engine/camera.h
+++ Engine/camera.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include "screen.h"
+#include <cmath>
 #include <glm/vec3.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 #include <glm/matrix.hpp>
 
 
@@ -10,20 +12,16 @@ namespace Engine {
     private:
         glm::vec3 location;
         glm::vec3 direction;
-        glm::mat4 transformMatrix;
-        glm::mat4 basisMatrix;
-        glm::mat4 translationMatrix;
-        void computeMatrix();
-        void computeBasis();
-        void computeTranslation();
-
+        glm::mat4 transformMatrix{};
+        glm::mat4 computeBasis();
+        static glm::mat4 computeTranslation(const glm::vec3& vec);
 
     public:
         Camera(glm::vec3 loc, glm::vec3 dir);
         Camera() = delete;
 
         void changeLocation(glm::vec3 delta);
-        void changeDirection(glm::vec3 delta);
+        void changeDirection(glm::vec3 deltaPhi);
 
         glm::mat4 getCameraTransformMatrix() const;
     };
