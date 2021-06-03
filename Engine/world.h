167bc1bbbc63243f396529567c60cdab60bb5812
jakokorina <73951854+jakokorina@users.noreply.github.com>
2021-06-03 14:24:30 +0300

    time added

--- Engine/world.h
+++ Engine/world.h
@@ -6,10 +6,14 @@
 #include "camera.h"
 #include "screen.h"
 #include "triangle.h"
-#include <math.h>
+#include <string>
+#include <iosfwd>
+#include <iostream>
 #include <vector>
+#include <filesystem>
 
 namespace Engine {
+
     class World {
     private:
         std::vector<Triangle> objects;
@@ -17,18 +21,16 @@ namespace Engine {
         Screen screen;
         Renderer renderer;
         sf::RenderWindow window;
-        void drawPicture();;
+        sf::Font font;
+        sf::Text text;
+        void drawPicture(const sf::Clock& clock);;
 
     public:
-        World(int width = 800, int height = 800, glm::vec3 camLoc = {0, 0, -1}, glm::vec3 camDir = {0, 0, 1}) :
-                camera(camLoc, camDir),
-                screen(height, width),
-                renderer(screen, camera),
-                window(sf::RenderWindow(sf::VideoMode(width, height), "Simple renderer")) {
-        }
+        World(int width = 800, int height = 800, glm::vec3 camLoc = {0, 0, -1}, glm::vec3 camDir = {0, 0, 1});
         void addObject(const std::vector<glm::vec3>& points, sf::Color colour);
         void addObject(const glm::vec3& p1, const glm::vec3& p2, const glm::vec3& p3, sf::Color colour);
-        void addObject(const glm::vec3& p1, const glm::vec3& p2, const glm::vec3& p3, const std::vector<sf::Color>& colour);
+        void
+        addObject(const glm::vec3& p1, const glm::vec3& p2, const glm::vec3& p3, const std::vector<sf::Color>& colour);
         void start();
         void keyPressed(sf::Keyboard::Key key);
     };
