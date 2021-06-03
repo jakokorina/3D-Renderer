27aaeadbfdad0c8ed3a790a8114e4c6b08e275b9
jakokorina <73951854+jakokorina@users.noreply.github.com>
2021-06-03 14:24:04 +0300

    time added

--- Engine/world.cpp
+++ Engine/world.cpp
@@ -1,6 +1,21 @@
+#include <sstream>
 #include "world.h"
 
 namespace Engine {
+    World::World(int width, int height, glm::vec3 camLoc, glm::vec3 camDir) :
+            camera(camLoc, camDir),
+            screen(height, width),
+            renderer(screen, camera),
+            window(sf::RenderWindow(sf::VideoMode(width, height), "Simple renderer")) {
+        std::string string = std::filesystem::current_path();
+        if (!font.loadFromFile(string + "/arial.ttf")) {
+            exit(1);
+        }
+        text.setFont(font);
+        text.setFillColor(sf::Color::White);
+        text.setCharacterSize(24);
+    }
+
     void World::addObject(const std::vector<glm::vec3>& points, sf::Color colour) {
         objects.emplace_back(points, colour);
     }
@@ -12,15 +27,22 @@ namespace Engine {
         objects.emplace_back(p1, p2, p3, colour);
     }
 
-    void World::drawPicture() {
+    void World::drawPicture(const sf::Clock& clock) {
         window.clear();
         screen.clear();
+        float before = clock.getElapsedTime().asSeconds();
         renderer.drawObjects(objects, window);
+        float time = clock.getElapsedTime().asSeconds() - before;
+        text.setString("Seconds per frame: " + std::to_string(time));
+        window.draw(text);
         window.display();
     }
 
     void World::start() {
-        drawPicture();
+        sf::Clock clock;
+        clock.restart();
+        drawPicture(clock);
+
         while (window.isOpen()) {
             sf::Event event;
             while (window.pollEvent(event)) {
@@ -28,7 +50,7 @@ namespace Engine {
                     window.close();
                 } else if (event.type == sf::Event::KeyPressed) {
                     keyPressed(event.key.code);
-                    drawPicture();
+                    drawPicture(clock);
                 }
             }
         }
@@ -36,22 +58,22 @@ namespace Engine {
     void World::keyPressed(sf::Keyboard::Key key) {
         switch (key) {
             case sf::Keyboard::A:
-                camera.changeLocation({-0.5, 0, 0});
+                camera.changeLocation({-0.2, 0, 0});
                 break;
             case sf::Keyboard::D:
-                camera.changeLocation({0.5, 0, 0});
+                camera.changeLocation({0.2, 0, 0});
                 break;
             case sf::Keyboard::W:
-                camera.changeLocation({0, 0.5, 0});
+                camera.changeLocation({0, 0.2, 0});
                 break;
             case sf::Keyboard::S:
-                camera.changeLocation({0, -0.5, 0});
+                camera.changeLocation({0, -0.2, 0});
                 break;
             case sf::Keyboard::X:
-                camera.changeLocation({0, 0, -0.5});
+                camera.changeLocation({0, 0, -0.2});
                 break;
             case sf::Keyboard::C:
-                camera.changeLocation({0, 0, 0.5});
+                camera.changeLocation({0, 0, 0.2});
                 break;
             case sf::Keyboard::Up:
                 camera.changeDirection({0, M_PI / 20, 0});
@@ -70,4 +92,5 @@ namespace Engine {
                 break;
         }
     }
+
 }
