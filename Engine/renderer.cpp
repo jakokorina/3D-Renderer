4942b726b24328164cef29af27a14ce85ed7ac26
jakokorina <73951854+jakokorina@users.noreply.github.com>
2021-06-09 21:56:31 +0300

    Update renderer.cpp

--- Engine/renderer.cpp
+++ Engine/renderer.cpp
@@ -2,16 +2,17 @@
 
 namespace Engine {
     void Renderer::drawObjects(const std::vector<Triangle>& objects, sf::RenderWindow& window) {
-        glm::mat4 transformMatrix =
-                screen.getScreenMatrix() * camera.getCameraTransformMatrix();
+        glm::mat4 screenTransformMatrix = screen.getScreenMatrix();
+        glm::mat4 cameraTransformMatrix = camera.getCameraTransformMatrix();
         for (const auto& object : objects) {
-            object.draw(screen, transformMatrix);
+            object.draw(screen, cameraTransformMatrix, screenTransformMatrix);
         }
+
         screen.screenToWindow(window);
         {
             auto[h, w] = screen.getSize();
             sf::RectangleShape pixel;
-            pixel.setSize({10.f, 10.f});
+            pixel.setSize({5.f, 5.f});
             pixel.setPosition({(float) w / 2, (float) h / 2});
             pixel.setFillColor(sf::Color::White);
             window.draw(pixel);
